6cb06c20326d524bc0d564b9a1ee6b3b73e8bd12
Artem Astapenko <jamakase54@gmail.com>
2022-08-04 22:25:52 +0300

    Remove generated code

--- examples/main.tf
+++ examples/main.tf
@@ -1,13 +1,10 @@
-#resource "airbyte_workspace" "default_workspace" {
-#  name  = "new-workspace"
-#  email = "test@jamakase.com"
-#  #  news = "new-workspace"
-#  #  securityUpdates = "new-workspace"
-#  #  notifications = "new-workspace"
-#}
-
-variable "workspace_id" {
-  default = "142d0f53-d4ec-4047-bf87-9110d982f399"
+resource "airbyte_workspace" "default_workspace" {
+  name  = "new-workspace"
+  email = "test@jamakase.com"
+  initialSetup = true
+  #  news = "new-workspace"
+  #  securityUpdates = "new-workspace"
+  #  notifications = "new-workspace"
 }
 
 data "airbyte_source_definition" "postgres" {
@@ -28,7 +25,7 @@ data "airbyte_destination_definition" "local" {
 
 resource "airbyte_source" "source_1" {
   name                 = "poke-source"
-  workspace_id         = var.workspace_id
+  workspace_id         = airbyte_workspace.default_workspace.workspace_id
   source_definition_id = data.airbyte_source_definition.pokemon.source_definition_id
 
   connection_configuration = jsonencode({
@@ -37,8 +34,8 @@ resource "airbyte_source" "source_1" {
 }
 
 resource "airbyte_destination" "destination_1" {
-  name                 = "destination-2"
-  workspace_id         = var.workspace_id
+  name                      = "destination-2"
+  workspace_id              = airbyte_workspace.default_workspace.workspace_id
   destination_definition_id = data.airbyte_destination_definition.local.destination_definition_id
 
   connection_configuration = jsonencode({
@@ -47,30 +44,33 @@ resource "airbyte_destination" "destination_1" {
 }
 #
 resource "airbyte_connection" "gvcmps_warehouse_pipeline" {
-  name = "GiveCampus app to Warehouse Pipeline"
-  source_id = airbyte_source.source_1.source_id
+  name           = "GiveCampus app to Warehouse Pipeline"
+  source_id      = airbyte_source.source_1.source_id
   destination_id = airbyte_destination.destination_1.destination_id
-  status = "active"
-#  namespace_definition = "destination"
-  schedule = {
-    units = 3
+  status         = "active"
+  #  namespace_definition = "destination"
+  schedule       = {
+    units     = 3
     time_unit = "hours"
   }
-#
-#  incremental_streams = {
-#    destination_sync_mode = "append_dedup"
-#    cursor_field = "updated_at"
-#    primary_key = "id"
-#    names = ["addtnl_fields", "affiliations", "contributions", "designations", ...]
-#  }
-#  incremental_streams = {
-#    destination_sync_mode = "append_dedup"
-#    cursor_field = "other_timestamp"
-#    primary_key = "id"
-#    names = ["subscriptions", "projects", "users", ...]
-#  }
-#  full_refresh_streams = {
-#    destination_sync_mode = "overwrite"
-#    names = "^(campaign_advocate([a-z_]+)|([a-z_]+)forms([a-z_]+)$"
-#  }
+  #
+  #  incremental_streams = {
+  #    destination_sync_mode = "append_dedup"
+  #    cursor_field = "updated_at"
+  #    primary_key = "id"
+  #    names = ["addtnl_fields", "affiliations", "contributions", "designations", ...]
+  #  }
+  #  incremental_streams = {
+  #    destination_sync_mode = "append_dedup"
+  #    cursor_field = "other_timestamp"
+  #    primary_key = "id"
+  #    names = ["subscriptions", "projects", "users", ...]
+  #  }
+  #  full_refresh_streams = {
+  #    destination_sync_mode = "overwrite"
+  #    names = "^(campaign_advocate([a-z_]+)|([a-z_]+)forms([a-z_]+)$"
+  #  }
+}
+output "workspace_id" {
+  value = airbyte_workspace.default_workspace.workspace_id
 }
